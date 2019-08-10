389919277e7ff5aac391cb909ddcf24d07662d40
lizhengxiang <1065482100@qq.com>
2019-08-10 17:06:31 +0800

    WithdrawalMargin.jsx

---
+++ src/components/WithdrawalMargin.jsx
@@ -0,0 +1,121 @@
+import React from 'react';
+import {Row, Col, Card, Table, Pagination, Tooltip,} from 'antd';
+import WithdrawDepositForm from './WithdrawDepositForm';
+import * as Api from '../apis';
+
+
+class WithdrawalMargin extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            deposit: 0,
+            getdepositlist: {
+                page: 1,
+                pageSize: 5,
+                data: [],
+                total: 0
+            },
+            getextractdepositlist: {
+                page: 1,
+                pageSize: 5,
+                data: [],
+                total: 0
+            },
+            depositBalance:0.00
+        }
+    }
+
+    componentWillMount() {
+        this.getextractDepositlist(this.state.getextractdepositlist.page, this.state.getextractdepositlist.pageSize);
+    }
+
+    componentWillUpdate() {
+    }
+
+    getextractDepositlist = (page, pageSize) => {
+        const token = localStorage.getItem('xpool-token');
+        Api.getextractDepositlist(token, page, pageSize).then(res => {
+            if (res.data.code) {
+                this.setState(preState => ({
+                    getextractdepositlist: Object.assign({}, preState.getextractdepositlist, {
+                        page: res.data.data.page,
+                        pageSize: res.data.data.pageSize,
+                        total: res.data.data.total,
+                        data: res.data.data.extract_deposit_list
+                    })
+                }))
+            }
+        })
+    };
+
+    onHandleChangeExtractDepositlistTable = (page, pageSize) => {
+        this.getextractDepositlist(page, pageSize);
+    }
+
+    render() {
+        const footer_getextractdepositlist = () => (
+            <Pagination total={this.state.getextractdepositlist.total} current={this.state.getextractdepositlist.page} pageSize={this.state.getextractdepositlist.pageSize} hideOnSinglePage={true} onChange={this.onHandleChangeExtractDepositlistTable}/>
+        )
+
+        const columns_getextractdepositlist = [{
+            title: 'ID',
+            dataIndex: 'ID',
+        }, {
+            title: 'Email',
+            dataIndex: 'Email',
+        },{
+            title: '取现金额',
+            dataIndex: 'Value',
+        },{
+            title: '审核状态',
+            dataIndex: 'State',
+            render: (text) => {
+                switch (text) {
+                    case 1:
+                        return ( <span>待审核</span> )
+                    case 3:
+                        return ( <span>审核通过</span> )
+                    case 5:
+                        return ( <span>审核拒绝</span> )
+                    default:
+                        break;
+                }
+            }
+        },{
+            title: '审核失败原因',
+            dataIndex: 'Reason',
+        },{
+            title: '申请添加时间',
+            dataIndex: 'CreatedAt',
+        }]
+
+        return(
+            <div>
+                <Row>
+                    <Col md={24} sm={24}>
+                        <Card style={{margin: 20}}>
+                            <Row >
+                                <Col md={24} sm={24}>
+                                    <WithdrawDepositForm onComplete={this.getextractDepositlist}/>
+                                </Col>
+                            </Row>
+                        </Card>
+                    </Col>
+
+                    <Col span={24}>
+                        <Card style={{margin: 20, marginTop: 0}}>
+                            <h3 style={{padding: 10}}>申请提现保证金列表</h3>
+                            <Table rowKey="ID"
+                                   dataSource={this.state.getextractdepositlist.data}
+                                   columns={columns_getextractdepositlist }
+                                   pagination={false}
+                                   footer={footer_getextractdepositlist}
+                            />
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
+}
+export default WithdrawalMargin;
